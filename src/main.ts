9b4e8a88607ed08a527277cb3b7774818631dbec
Alfons-ES <alfonsersv@gmail.com>
2026-03-27 10:36:14 +0100

    fungerande localstorage

--- src/main.ts
+++ src/main.ts
@@ -8,9 +8,36 @@ interface CourseInfo {
   syllabus: string;
 }
 
+const output = document.getElementById("outputTable") as HTMLTableElement;
+
+loadCourses();
+
+function loadCourses(): void {
+  const savedCourses = localStorage.getItem("courses");
+
+  if (!savedCourses) {
+    return;
+  }
+
+  const courses: CourseInfo[] = JSON.parse(savedCourses);
+
+  courses.forEach(course => {
+
+    output.innerHTML += `
+  <tr>
+    <td>${course.code}</td>
+    <td>${course.name}</td>
+    <td id="tdcentre">${course.progression}</td>
+    <td id="tdcentre"><a href="${course.syllabus}" target="_blank">länk</td>
+  </tr>
+  `
+
+  });
+
+}
 //när sidan laddas in hämtas allt från localstorage och läggs ut på sidan.
 
-(document.getElementById("add") as HTMLButtonElement).addEventListener("click", (e: Event) => {
+(document.getElementById("add") as HTMLButtonElement).addEventListener("click", () => {
 
   console.log("knapptryck")
 
@@ -20,18 +47,23 @@ interface CourseInfo {
   const tempProgression: string = (document.getElementById("progression") as HTMLInputElement).value;
   const tempSyllabus: string = (document.getElementById("syllabus") as HTMLInputElement).value;
 
+
+  const feedback = document.getElementById("feedback") as HTMLTextAreaElement
+
   if (tempCode === "" || tempName === "" || tempProgression === "" || tempSyllabus === "") {
     console.log("fyll i alla rutor")
+    feedback.innerHTML = "fyll i alla rutor!"
     return;
   } else if (tempProgression !== "A" && tempProgression !== "B" && tempProgression !== "C") {
     console.log("felaktig progression")
+    feedback.innerHTML = "Progressionen måste vara A, B eller C"
     return;
   } else {
     console.log("ifyllt korrekt")
   }
 
 
-  const course: Partial<CourseInfo> = {
+  const course: CourseInfo = {
     code: tempCode,
     name: tempName,
     progression: tempProgression as "A" | "B" | "C",
@@ -40,21 +72,24 @@ interface CourseInfo {
 
   console.log(course)
 
-  const output = (document.getElementById("outputTable") as HTMLTableElement)
+
 
 
   output.innerHTML += `
   <tr>
     <td>${course.code}</td>
     <td>${course.name}</td>
-    <td>${course.progression}</td>
-    <td>${course.syllabus}</td>
+    <td id="tdcentre">${course.progression}</td>
+    <td id="tdcentre"><a href="${course.syllabus}" target="_blank">länk</td>
   </tr>
   `
 
 
+  let courses: CourseInfo[] = JSON.parse(localStorage.getItem("courses") || "[]");
 
-  //lägg till Input i localstorage
+  courses.push(course);
+
+  localStorage.setItem("courses", JSON.stringify(courses));
 
 
 });
